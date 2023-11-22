a016e25657a1fbe95451be6303cd9f1df4013fc2
Sivasurya002 <114906231+Sivasurya002@users.noreply.github.com>
2023-11-22 22:22:02 +0530

    Update Ptr1.java

--- Ptr1.java
+++ Ptr1.java
@@ -1,18 +1,10 @@
-package nov17;
-
-import java.util.Scanner;
-
-class Ptr1 {
-	public static void main (String[] args) {
-		Scanner sc = new Scanner (System.in);
-		System.out.print("Enter value for n:");
-		int n = sc.nextInt();
-		
-		for(int i=1; i<=n; i++) {
-			for(int j=n; j>=i; j--) {
-				System.out.print(j+" ");
-			}
-			System.out.println();
-		}
+class Ptr1{
+	public static void main(String[] args) {
+		int a = 1, b = 2, c=3;
+		int d = ++a + ++c - ++b + a++ - ++c + ++b -b++;
+		System.out.println("a :"+a);
+		System.out.println("b :"+b);
+		System.out.println("c :"+c);
+		System.out.println("d :"+d);
 	}
 }
